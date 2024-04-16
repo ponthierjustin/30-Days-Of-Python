b68bf49d37b836d97785576b7554b35c6a2f10f7
Justin <ponthierjustin@gmail.com>
2024-04-15 21:34:29 -0400

    adding Python Data Types

--- day_1.py
+++ day_1.py
@@ -6,11 +6,20 @@ print(3 % 4) #modulus
 print(3 / 4) #divison
 print(3 ** 4) #exponential
 print(3 // 4) #floor division
+print("\n")
 
 # Python Strings
 print("Justin Ponthier") # Your name
 print("Justin") # Your family name
 print("United States of America") # Your Country
-print("I am enjoying 30 days of python") # I am enjoying 30 days of python
+print("I am enjoying 30 days of python \n") # I am enjoying 30 days of python
 
 # Python Data Types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Justin Ponthier"))
+print(type("Justin"))
+print(type("United States of America"))
