fad43441226cff0ac037a26aa8b57501d941901a
Justin <ponthierjustin@gmail.com>
2024-04-15 21:25:09 -0400

    adding pythong operators and strings

--- day_1.py
+++ day_1.py
@@ -1,1 +1,14 @@
-print("Hello World")
+# Python Operations
+print(3 + 4) #addition
+print(3 - 4) #subtraction
+print(3 * 4) #multiplication
+print(3 % 4) #modulus
+print(3 / 4) #divison
+print(3 ** 4) #exponential
+print(3 // 4) #floor division
+
+# Python Strings
+print("Justin Ponthier")
+print("Justin")
+print("United States of America")
+print("I am enjoying 30 days of python")
