8d603609f6cb7d2befc8235466209f7b4278702e
Justin <ponthierjustin@gmail.com>
2024-04-15 21:27:01 -0400

    adding comments for string

--- day_1.py
+++ day_1.py
@@ -8,7 +8,9 @@ print(3 ** 4) #exponential
 print(3 // 4) #floor division
 
 # Python Strings
-print("Justin Ponthier")
-print("Justin")
-print("United States of America")
-print("I am enjoying 30 days of python")
+print("Justin Ponthier") # Your name
+print("Justin") # Your family name
+print("United States of America") # Your Country
+print("I am enjoying 30 days of python") # I am enjoying 30 days of python
+
+# Python Data Types
