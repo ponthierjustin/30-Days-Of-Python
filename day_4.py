fb16cc993672744cc513628930368e361e2ac3ce
Justin <ponthierjustin@gmail.com>
2024-04-18 13:24:34 -0400

    pushing q 1-8

---
+++ day_4.py
@@ -0,0 +1,32 @@
+# Justin
+# 4/18/24
+
+# Q1
+thirty_days_of_python_string = ['Thirty', 'Days', 'Of', 'Pyhton']
+result = ' '.join(thirty_days_of_python_string)
+print(result)
+
+# Q2
+coding_for_all_string = ['Coding', 'For', 'All']
+result = ' '.join(coding_for_all_string)
+print(result)
+
+# Q3
+company = 'Coding For All' 
+
+# Q4
+print(company)
+
+# Q5
+print(len(company))
+
+# Q6
+print(company.upper())
+
+# Q7
+print(company.lower())
+
+# Q8
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import math
 #Testing code
-print(' \n 1 1 1 1 1 \n 2 1 2 4 8 \n 3 1 3 9 27 \n 4 1 4 16 64 \n 5 1 5 25 125')
+thirty_days_of_python_string = ['Thirty', 'Days', 'Of', 'Pyhton']
+result = ' '.join(thirty_days_of_python_string)
+print(result)
