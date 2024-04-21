e62115e12f1bfdec3af4fab711acda6ced5daa58
Justin <ponthierjustin@gmail.com>
2024-04-21 17:02:09 -0400

    pushing day 6 files and all exercises

--- day_6.py
+++ day_6.py
@@ -15,4 +15,33 @@ family_members = siblings + ('Dad', "Mom")
 
 print(family_members)
 
+siblings = family_members[0:9]
+parents = family_members[-2:]
 
+print(siblings)
+print(parents)
+
+fruits = ('banana', 'orange', 'mango')
+vegetables = ('Asparagus', 'Cauliflower', 'Green Bean')
+animals = ('Dog', 'Cat', 'Bunny')
+
+food_stuff_tp = fruits + vegetables + animals
+
+print(food_stuff_tp)
+
+food_stuff_lt = list(food_stuff_tp)
+
+print(food_stuff_lt)
+
+middle_index = int(len(food_stuff_lt)/ 2) - 1
+print(middle_index)
+print(food_stuff_lt[middle_index])
+
+print(food_stuff_lt[0:3])
+
+print(food_stuff_lt[6:9])
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+
+print('Estonia' in nordic_countries)
+print('Iceland' in nordic_countries)
