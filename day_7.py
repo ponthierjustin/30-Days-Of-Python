c00a5ca0cd64b7c7cb6d3c807bc26a40dbe60857
Justin <ponthierjustin@gmail.com>
2024-04-21 17:11:52 -0400

    pushing day 7 exercise 1

--- day_7.py
+++ day_7.py
@@ -1,3 +1,26 @@
 # Justin    
 # 4/21/24
 
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+
+
+print(len(it_companies))
+
+it_companies.add('Twitter')
+
+print(it_companies)
+
+it_companies.update(['Computacenter', 'Accenture', 'Nvidia'])
+
+print(it_companies)
+
+it_companies.remove('Nvidia')
+
+print(it_companies)
+
+#Discard will not raise an error even if the item is not found
+
