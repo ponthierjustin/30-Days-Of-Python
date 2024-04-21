3b37dc70d5d7d40f31e13f646d4c1a182cb18453
Justin <ponthierjustin@gmail.com>
2024-04-21 16:39:35 -0400

    pushing day 6 exercise 1

---
+++ day_6.py
@@ -0,0 +1,18 @@
+# Justin
+# 4/21/24
+
+empty_tuple = ()
+
+brothers = ('Bradley', 'Bond', 'Adnan', 'Murtaza', 'Chase')
+
+sisters = ('Jessica', 'Isabella', 'Brooklyn', 'Savannah')
+
+siblings = brothers + sisters
+
+print(len(siblings))
+
+family_members = siblings + ('Dad', "Mom")
+
+print(family_members)
+
+
