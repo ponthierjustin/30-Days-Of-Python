d43d69d4db0e2b26f066db806d36e622ea5f74cb
Justin <ponthierjustin@gmail.com>
2024-05-07 20:27:39 -0400

    pushing day 17.py exercise

---
+++ day_17.py
@@ -0,0 +1,12 @@
+# 5/7/24
+# Justin
+
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+
+nordic_countries = names[:5]
+
+es, ru = names[5], names[6]
+
+print("Nordic countries:", nordic_countries)
+print("Estonia:", es)
+print("Russia:", ru)
